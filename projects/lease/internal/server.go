b0c5ffe295dfbd310b1ec910bb4e4be9165055e9
Oleg Stotsky <o.stotskiy@corp.vk.com>
2021-12-25 19:24:43 +0300

    [lease-service] add auth check

--- projects/lease/internal/server.go
+++ projects/lease/internal/server.go
@@ -2,6 +2,8 @@ package internal
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/golang-jwt/jwt"
 	"go.uber.org/zap"
 	"io/ioutil"
 	"net/http"
@@ -14,15 +16,16 @@ type HttpServer struct {
 	jwtSigningKey []byte
 }
 
-func NewHttpServer(addr string, leaseService *LeaseService, logger *zap.SugaredLogger) *HttpServer {
+func NewHttpServer(addr string, leaseService *LeaseService, logger *zap.SugaredLogger, jwtSecret []byte) *HttpServer {
 	srv := http.Server{
 		Addr: addr,
 	}
 
 	httpServer := HttpServer{
-		server:       srv,
-		leaseService: leaseService,
-		logger:       logger,
+		server:        srv,
+		leaseService:  leaseService,
+		logger:        logger,
+		jwtSigningKey: jwtSecret,
 	}
 
 	mux := http.NewServeMux()
@@ -33,8 +36,7 @@ func NewHttpServer(addr string, leaseService *LeaseService, logger *zap.SugaredL
 }
 
 type createLeaseRequest struct {
-	UserID uint64 `json:"user_id"`
-	CarID  uint64 `json:"car_id"`
+	CarID uint64 `json:"car_id"`
 }
 
 type createLeaseResponse struct {
@@ -47,12 +49,24 @@ func (c *HttpServer) ListenAndServe() error {
 	return c.server.ListenAndServe()
 }
 
+type UserAuthObject struct {
+	Username string
+	UserID   uint64
+}
+
 func (c *HttpServer) Close() error {
 	return c.server.Close()
 }
 
 func (c *HttpServer) createLease(rw http.ResponseWriter, r *http.Request) {
 	c.logger.Infof("got request for create lease")
+	token := r.Header.Get("X-Auth")
+	userAuth, err := c.checkAuth(token)
+	if err != nil {
+		c.logger.Errorf("auth error: %v", err)
+		http.Error(rw, err.Error(), http.StatusUnauthorized)
+		return
+	}
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -69,7 +83,7 @@ func (c *HttpServer) createLease(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	lease, err := c.leaseService.createLease(createLeaseRequest.UserID, createLeaseRequest.CarID)
+	lease, err := c.leaseService.createLease(userAuth.UserID, createLeaseRequest.CarID)
 	if err != nil {
 		if err == leaseAlreadyExists {
 			c.logger.Errorf("create lease error: lease with car_id %v already exists", createLeaseRequest.CarID)
@@ -97,4 +111,26 @@ func (c *HttpServer) createLease(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		c.logger.Errorf("create lease error: error writing response %v", err)
 	}
+}
+
+func (c *HttpServer) checkAuth(token string) (UserAuthObject, error) {
+	tokenObj, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return c.jwtSigningKey, nil
+	})
+	if err != nil {
+		return UserAuthObject{}, fmt.Errorf("error casting user id from token: %w", err)
+	}
+
+	claims, ok := tokenObj.Claims.(jwt.MapClaims)
+	if !ok {
+		return UserAuthObject{}, fmt.Errorf("token %s verification error: error casting claims to map claims", token)
+	}
+	c.logger.Infof("claims %v", claims)
+	username := claims["username"].(string)
+	userID := claims["user_id"].(float64)
+
+	return UserAuthObject{
+		username,
+		uint64(userID),
+	}, nil
 }
